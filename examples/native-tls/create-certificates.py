48f2244bfce1e616855ca792296ea45a049c1ada
Benno Evers <benno.evers@tenzir.com>
2025-01-30 12:59:24 +0100

    some fixups

--- examples/native-tls/create-certificates.py
+++ examples/native-tls/create-certificates.py
@@ -19,7 +19,7 @@ ca = trustme.CA()
 ca.cert_pem.write_to_path("ssl/ca.pem")
 
 # Get the DNS names for the certificates.
-frontend_endpoint = urlparse(os.getenv("TENZIR_PLATFORM_DOMAIN")).hostname
+app_endpoint = urlparse(os.getenv("TENZIR_PLATFORM_DOMAIN")).hostname
 platform_endpoint = urlparse(os.getenv("TENZIR_PLATFORM_API_ENDPOINT")).hostname
 control_endpoint = urlparse(os.getenv("TENZIR_PLATFORM_CONTROL_ENDPOINT")).hostname
 blobs_endpoint = urlparse(os.getenv("TENZIR_PLATFORM_BLOBS_ENDPOINT")).hostname
@@ -39,14 +39,9 @@ def write_certificate(server_names: list[str], filename: str) -> None:
 # inside the container network. For a production setup, this
 # might not be possible, in this case all traffic must
 # be routed through the external network.
-write_certificate([frontend_endpoint, "app", "localhost"], filename="ssl/frontend-cert.pem")
+write_certificate([app_endpoint, "app", "localhost"], filename="ssl/app-cert.pem")
 write_certificate([platform_endpoint, "platform"], filename="ssl/platform-cert.pem")
 write_certificate([control_endpoint, "websocket-gateway"], filename="ssl/control-cert.pem")
 write_certificate([blobs_endpoint, "seaweed"], filename="ssl/blobs-cert.pem")
 write_certificate([login_endpoint, "keycloak"], filename="ssl/login-cert.pem")
 write_certificate(["postgres"], filename="ssl/db-cert.pem")
-
-
-# Postgres is particularly picky 
-os.chmod("ssl/db-cert.pem", 0o600)
-os.chown("ssl/db-cert.pem", 999, 999)
