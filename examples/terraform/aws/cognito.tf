9fa6030e428dd43df33485618b929cafa507ac13
Benno Evers <benno.evers@tenzir.com>
2025-08-14 11:30:50 +0200

    Add UI customization for Cognito hosted login page
    - Added aws_cognito_user_pool_ui_customization resource
    - Configured CSS styling for login page elements
    - Applied to tenzir-app client with default styling options

--- examples/terraform/aws/cognito.tf
+++ examples/terraform/aws/cognito.tf
@@ -80,6 +80,85 @@ resource "aws_cognito_user_pool_domain" "tenzir" {
   user_pool_id = aws_cognito_user_pool.tenzir.id
 }
 
+# UI customization for the hosted login page
+resource "aws_cognito_user_pool_ui_customization" "tenzir" {
+  user_pool_id = aws_cognito_user_pool_domain.tenzir.user_pool_id
+  client_id    = aws_cognito_user_pool_client.app_client.id
+  
+  # CSS customization for login page
+  css = <<-EOF
+    .banner-customizable {
+      padding: 25px 0px 25px 0px;
+      background-color: #2d3142;
+    }
+    .label-customizable {
+      font-weight: 400;
+    }
+    .textDescription-customizable {
+      padding-top: 10px;
+      padding-bottom: 10px;
+      display: block;
+      font-size: 16px;
+    }
+    .idpDescription-customizable {
+      padding-top: 10px;
+      padding-bottom: 10px;
+      display: block;
+      font-size: 16px;
+    }
+    .legalText-customizable {
+      color: #747474;
+      font-size: 11px;
+    }
+    .submitButton-customizable {
+      font-size: 14px;
+      font-weight: bold;
+      margin: 20px 0px 10px 0px;
+      height: 40px;
+      width: 100%;
+      color: #fff;
+      background-color: #4889f4;
+    }
+    .submitButton-customizable:hover {
+      color: #fff;
+      background-color: #3d71d9;
+    }
+    .errorMessage-customizable {
+      padding: 5px;
+      font-size: 14px;
+      width: 100%;
+      background: #F5F5F5;
+      border: 2px solid #D64958;
+      color: #D64958;
+    }
+    .inputField-customizable {
+      height: 34px;
+      width: 100%;
+      color: #555;
+      background-color: #fff;
+      border: 1px solid #ccc;
+    }
+    .inputField-customizable:focus {
+      border-color: #66afe9;
+      outline: 0;
+    }
+    .idpButton-customizable {
+      height: 40px;
+      width: 100%;
+      text-align: center;
+      margin-bottom: 15px;
+      color: #fff;
+      background-color: #5bc0de;
+      border-color: #46b8da;
+    }
+    .idpButton-customizable:hover {
+      color: #fff;
+      background-color: #31b0d5;
+      border-color: #269abc;
+    }
+  EOF
+}
+
 # Amazon identity provider
 resource "aws_cognito_identity_provider" "amazon" {
   user_pool_id  = aws_cognito_user_pool.tenzir.id
