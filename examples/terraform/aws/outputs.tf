4f648bb5a0880d2582bfbeb77a326b3c2739aa6e
Benno Evers <benno.evers@tenzir.com>
2025-08-14 12:08:05 +0200

    Rename admin password output and remove sensitive flag

--- examples/terraform/aws/outputs.tf
+++ examples/terraform/aws/outputs.tf
@@ -71,8 +71,7 @@ output "admin_username" {
 
 output "admin_initial_password" {
   description = "Initial admin password for Cognito (change after first login)"
-  value       = random_password.admin_password.result
-  sensitive   = true
+  value       = nonsensitive(random_password.admin_password.result)
 }
 
 
